0e9c6bc5e82d8981d4d5b6783103b8ac0560cb5f
corvo-fly <ravenawakened0413@gmail.com>
2025-11-22 13:11:55 +0800

    update my-portfolio

---
+++ app/home/page.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+
+const Homepage = ()=>{
+    return <div className="bg-white h-[100vh] w-full flex items-center justify-center">
+
+        <div className="flex items-center justify-center flex-col ">
+            <Image
+            src="/profile.png"
+            width={500}
+            height={500}
+            alt="profile"
+            />
+        </div>
+        <br></br>
+        <h2 className="text-black text-[60px] font-bold">My oshi</h2>
+        <p className="max-w-[500px] w-full text-black text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam reprehenderit laudantium, porro quae eius sapiente unde iure non quibusdam sed tempora tenetur et rem aliquam consequuntur earum accusamus voluptatem consectetur!</p>
+    </div>
+}
+export default Homepage
