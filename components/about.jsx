d555400e4d70ff13e93b674b1eadce4305eb8b3c
corvo-fly <ravenawakened0413@gmail.com>
2025-11-25 19:51:47 +0800

    update logos

--- components/about.jsx
+++ components/about.jsx
@@ -38,29 +38,88 @@ export default function About() {
 
             <div className="space-y-6">
 
-                <div className="border border-neutral-800 p-6">
-                    <p className="text-emerald-400 font-medium mb-2">🎓 Education</p>
-
+                <div className="p-6 border border-neutral-800">
+                    <h3 className="text-emerald-300 font-semibold mb-2 flex items-center gap-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-5 h-5 text-emerald-400"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth={1.5}
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M12 14.25L3.75 9.75 12 5.25l8.25 4.5-8.25 4.5z"
+                            />
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M6.75 10.5v4.5c0 .621.504 1.125 1.125 1.125h8.25A1.125 1.125 0 0017.25 15v-4.5"
+                            />
+                        </svg>
+                    Education
+                    </h3>
                     <p className="text-sm text-neutral-400">2022 - PRESENT</p>
-                    <p className="text-neutral-100 font-semibold">Central Philippine University</p>
-
-                    <p className="text-neutral-400 text-sm mb-4"> Bachelor of Science in Information Technology</p>
+                    <p className="font-medium">Central Philippine University</p>
+                    <p className="text-sm text-neutral-300">
+                        Bachelor of Science in Information Technology
+                    </p>
                 </div>
 
-                <div className="border border-neutral-800 p-6">
-                    <p className="text-emerald-400 font-medium mb-2">📍 Location</p>
-                    <p className="text-neutral-300">Iloilo, Philippines</p>
+                <div className="p-6 border border-neutral-800">
+                    <h3 className="text-emerald-300 font-semibold mb-2 flex items-center gap-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-5 h-5 text-emerald-400"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth={1.5}
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M12 11.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z"
+                            />
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M12 21c4-4.5 6-7.5 6-10.5a6 6 0 10-12 0c0 3 2 6 6 10.5z"
+                            />    
+                        </svg>
+                        Location
+                    </h3>
+                <p className="text-neutral-300">Iloilo, Philippines</p>
                 </div>
 
-                <div className="border border-neutral-800 p-6">
-                    <p className="text-emerald-400 font-medium mb-2">🗣 Languages</p>
+                <div className="p-6 border border-neutral-800">
+                    <h3 className="text-emerald-300 font-semibold mb-2 flex items-center gap-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-5 h-5 text-emerald-400"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth={1.5} 
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M3 5.25h6M6 3v4.5m-3 4.5h4.5A2.25 2.25 0 019.75 14.25v0A2.25 2.25 0 017.5 16.5H6l2.25 4.5M13.5 5.25h7.5m-3.75-2.25v4.5M15 21l2.25-4.5"
+                            />
+                        </svg>
+                        Languages
+                    </h3>
 
-                    <p className="text-neutral-100 font-semibold">English</p>
-                    <p className="text-neutral-400 text-sm mb-3">Fluent</p>
+                    <p className="font-medium">English</p>
+                    <p className="text-sm text-neutral-400">Fluent</p>
 
-                    <p className="text-neutral-100 font-semibold"> Filipino</p>
-                    <p className="text-neutral-400 text-sm mb-3">Fluent</p>
+                    <p className="font-medium mt-2">Filipino</p>
+                    <p className="text-sm text-neutral-400">Fluent</p>
                 </div>
+
             </div>
         </div>
     </section>
